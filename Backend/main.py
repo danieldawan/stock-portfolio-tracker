6d12aa553a9fc5b05b9b0b2982444b535919cb39
Daniel Dawan <141849615+danieldawan@users.noreply.github.com>
2024-03-03 17:01:15 +0100

    Created frontend and minor changes

--- Backend/main.py
+++ Backend/main.py
@@ -9,17 +9,19 @@ CORS(app)
 client_ticker = []
 
 def portfolio():
-    with open("dummy_data/portfolio.json") as json_file:
+    # Load portfolio data from JSON file
+    with open("./backend/portfolio.json") as json_file:
         data = json.load(json_file)
     
     return data
 
 @app.route('/', methods=["GET"])
 def homepage():
-
+    # Get client stocks from portfolio
     client_stocks = portfolio()
     stock_dict    = {}
 
+    # Create a dictionary of stock tickers and quantities
     for item in client_stocks["portfolios"][0]["items"]:
         stock_dict[item["ticker"]] = item["quantity"]
     
@@ -27,15 +29,17 @@ def homepage():
 
 @app.route('/<ticker>', methods=["GET"])
 def ticker_info(ticker):
-
     try:
         apikey = "2Q8AT87UOUTGOPGY"
         stock = ticker
         url = f"https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol={stock}&apikey={apikey}"
         
+        # Make API request to get stock information
         response = requests.get(url)
         data = response.json()
         stock_info = data["Weekly Time Series"]
+        
+        # Select the first 10 items from the stock information
         selected_items = list(stock_info.items())[:10]
         stock_info = dict(selected_items)
         print(stock_info)
